d51fdf0020b1663c74c53b2b8d3244a5f0be4f2b
Tom <Tom@WINDOWSPC>
2016-08-18 16:49:05 -0500

    Created a real Hello World console app in C#.  Reads user input, spits it back with a date and timestamp.

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World!");
+            Console.Write("\r\nPlease type something in: ");
+            string msg = Console.ReadLine();
+            Console.WriteLine("\r\nThank you for typing.  At " 
+                + DateTime.Now.ToShortDateString() + " " 
+                + DateTime.Now.ToShortTimeString() + " you wrote: " + msg);
+            Console.WriteLine("\r\nPress the return key to close this application.");
+            msg = Console.ReadLine();
+            //effectively pauses and ends the app.
+        }
+    }
+}
