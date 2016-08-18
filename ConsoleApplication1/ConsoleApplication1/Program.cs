3db3499ff2c16589517cd653b84d311c5a72afcc
Tom <Tom@WINDOWSPC>
2016-08-18 16:58:40 -0500

    Tweaked text instructions displayed in console, added spaces where needed, clarifications, etc.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,11 +11,11 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.Write("Hello World!");
-            Console.Write("\r\nPlease type something in: ");
+            Console.Write("\r\nPlease type something in and hit the Return key: ");
             string msg = Console.ReadLine();
             Console.WriteLine("\r\nThank you for typing.  At " 
                 + DateTime.Now.ToShortDateString() + " " 
-                + DateTime.Now.ToShortTimeString() + " you wrote: " + msg);
+                + DateTime.Now.ToShortTimeString() + " you typed: " + msg);
             Console.WriteLine("\r\nPress the return key to close this application.");
             msg = Console.ReadLine();
             //effectively pauses and ends the app.
